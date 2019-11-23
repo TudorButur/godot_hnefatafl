e1fdaa13372897388126479503a6281b68680007
Invader Zim <AustralianZim@yahoo.com>
2019-11-23 14:36:58 +1100

    First playable version!

--- Scripts/Soldier.gd
+++ Scripts/Soldier.gd
@@ -1,9 +1,21 @@
-extends Area2D
+extends KinematicBody2D
 
+var is_picked = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			is_picked = event.pressed
+			print("erm")
 
+func _physics_process(delta):
+	var movement: Vector2
 
+	if is_picked:
+		movement = get_global_mouse_position() - position
+
+	move_and_collide(movement)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,25 +1,38 @@
 extends Node2D
 
 var window = OS.get_window_safe_area()
-var size = 64
-var grid_width = 12
-var grid_height = 7 #Breaks if even
-var origin = [(1024-size*grid_width)/2,(600-size*grid_height)/2]
+var size = 96
+var grid_width = 9
+var grid_height = 9 #Breaks if even
+var origin = Vector2((1024-size*grid_width)/2,(1000-size*grid_height)/2)
 var colour1 = Color(0.337,0.769,0.200)
 var colour2 = Color(0.816,0.976,0.647)
-var squares = []
 
-var assets = [ preload("res://Scenes/Soldier.tscn")
-]
+
+
+var layout =[[0,0,0,1,1,1,0,0,0],
+			 [0,0,0,0,1,0,0,0,0],
+			 [0,0,0,0,2,0,0,0,0],
+			 [1,0,0,0,2,0,0,0,1],
+			 [1,1,2,2,3,2,2,1,1],
+			 [1,0,0,0,2,0,0,0,1],
+			 [0,0,0,0,2,0,0,0,0],
+			 [0,0,0,0,1,0,0,0,0],
+			 [0,0,0,1,1,1,0,0,0]]
+
+var assets = [preload("res://Scenes/Soldier.tscn")]
+var sprites = [0, 
+preload("res://media/pawn_black.png"),
+preload("res://media/grenadier.png"),
+preload("res://media/king.png")]
 
 # Called when the node enters the scene tree for the first time.
 func _draw():
-	print(window)
 	var cond = true
 	for i in range(grid_width):
 		for j in range(grid_height):
 			
-			var rect = Rect2(Vector2(origin[0]+i*size,origin[1]+j*size), Vector2(size,size))
+			var rect = Rect2(Vector2(origin.x+i*size,origin.y+j*size), Vector2(size,size))
 			
 			var colour
 			if cond:
@@ -29,16 +42,20 @@ func _draw():
 
 			draw_rect(rect, colour)
 			cond = not cond
-			
-	print(squares)
+		
 	pass # Replace with function body.
 
 func _ready():
-     var soldier = assets[0].instance()
-     add_child(soldier)
-
-     soldier.position = Vector2(20, 20)
-     
+	for i in range(9):
+		for j in range(9):
+			if layout[i][j] == 0:
+				continue
+			else:
+				var soldier = assets[0].instance()
+				add_child(soldier)
+				soldier.position = Vector2(origin.x + j*size + 48, origin.y + i*size + 48)
+				var sprite = soldier.get_node("Sprite")
+				sprite.set_texture(sprites[layout[i][j]]) 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
